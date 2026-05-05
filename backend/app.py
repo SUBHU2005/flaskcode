937726da872c192dd8097b58d2d79db7acb15633
Subham Sirohi <subham.sirohi.1234@gmail.com>
2026-05-05 22:37:25 +0530

    fist commit

---
+++ backend/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+MONGO_URI = "mongodb+srv://subhamsirohi1234_db_user:YgICVArL7kbkaGVD@cluster0.gup4qz7.mongodb.net/?appName=Cluster0"
+
+client = MongoClient(MONGO_URI)
+db = client["test_db"]
+collection = db["users"]
+
+@app.route('/')
+def form():
+    return render_template('form.html', error=None)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    try:
+        name = request.form.get('name')
+        email = request.form.get('email')
+
+        if not name or not email:
+            return render_template('form.html', error="All fields are required")
+
+        collection.insert_one({
+            "name": name,
+            "email": email
+        })
+
+        return redirect(url_for('success'))
+
+    except Exception as e:
+        return render_template('form.html', error=str(e))
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
